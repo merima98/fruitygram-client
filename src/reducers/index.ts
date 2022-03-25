2d54429b76d46b71e8eafe36923699a7e26641f9
Merima Ćeranić <55186271+merima98@users.noreply.github.com>
2022-03-25 11:55:26 +0100

    Establishment of architecture (#3)
    * Implemented fetch word functionality.
    
    * Changed name of file.

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { reducerWord } from "./words";
+import { reducerWord } from "./wordReducer";
 
 const rootReducer = combineReducers({
     reducerWord: reducerWord,
